f7cbafe3d9b498fb7075711230ff315a2219b3e6
ALEXANDRE LIMA <12657752810@info.local>
2026-03-21 09:46:09 -0300

    atualização 01 dia 21/03

--- src/Favoritos/favoritos.jsx
+++ src/Favoritos/favoritos.jsx
@@ -4,56 +4,53 @@ import Header from "../Header";
 import Footer from "../Footer";
 import { Container, MovieList,  Movie } from "../style";
 
-function Favorites(){
-    //Variável/Gaveta para guardar os item que estão no Local Storage
-     const [favs, setFavs] = useState([])
+function Favorites() {
+  const [favs, setFavs] = useState([]);
+  const img_path = 'https://image.tmdb.org/t/p/w500/';
 
+  useEffect(() => {
+    const savedFavs = JSON.parse(localStorage.getItem('favorites')) || []; // Fallback para array vazio
+    setFavs(savedFavs);
+  }, []);
 
-     const img_path = 'https://image.tmdb.org/t/p/w500/'
+  // O ERRO ESTAVA AQUI: Adicione o (id) como parâmetro
+  function removeFavorite(id) {
+    const updateFavs = favs.filter(movie => movie.id !== id);
+    setFavs(updateFavs);
+    localStorage.setItem('favorites', JSON.stringify(updateFavs));
+  }
 
-
-     useEffect(()=>{
-        const savedFavs = JSON.parse(localStorage.getItem('favorites'))
-        setFavs(savedFavs)
-     },[])
-
-     function removeFavorite(){
-        const updateFavs = favs.filter(movie => movie.id !== id)
-        setFavs(updateFavs)
-        localStorage.setItem('favorites',JSON.stringify(updateFavs))
-     }
-
-     
-return(<>
-<Header/>
-<Container>
-    {favs.length === 0 ?(
-        <p style={{textAlign:'center', marginTop:'3rem', color:'#aaa'}}>
+  return (
+    <>
+      <Header />
+      <Container>
+        {favs.length === 0 ? (
+          <p style={{ textAlign: 'center', marginTop: '15rem', color: '#aaa', fontSize: '32px' }}>
             Você ainda não salvou nenhum item como favorito!
-        </p>
-
-    ):(
-    <MovieList>
-        {favs.map(movie => 
-            (<Movie key={movie.id}>
+          </p>
+        ) : (
+          <MovieList>
+            {favs.map(movie => (
+              <Movie key={movie.id}>
                 <Link to={`/details/movie/${movie.id}`}>
-                <img src={`${img_path}${movie.poster}`}
-                alt={movie.title}/>
+                  <img src={`${img_path}${movie.poster}`} alt={movie.title} />
                 </Link>
-              <span>{movie.title}</span>  
+                <span>{movie.title}</span>
 
-              <button className="favorite-btn active"
-              onClick={()=> removeFavorite(movie.id)}
-              >Remover
-              </button>
-            </Movie>))}
-    </MovieList>
-    )}
-</Container>
-<Footer/>
-</>)
-    
+                <button 
+                  className="favorite-btn active" 
+                  onClick={() => removeFavorite(movie.id)} // Aqui você já chamava certo!
+                >
+                  Remover
+                </button>
+              </Movie>
+            ))}
+          </MovieList>
+        )}
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default Favorites
-
